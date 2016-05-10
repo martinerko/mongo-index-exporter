978d29cfc3303b3bb3778a3546111c3e0ca9b50f
martinerko <martinerko@gmail.com>
2016-05-10 20:05:29 +1000

    Sample added.

---
+++ sample/exporter.js
@@ -0,0 +1,24 @@
+/*!
+ * mongo-index-exporter
+ * Copyright(c) 2016 martinerko <martinerko@gmail.com>
+ * MIT Licensed
+ */
+
+/**
+ * Module dependencies.
+ */
+
+var exporter = require("../lib/exporter");
+var connectionString = "localhost:27017/test";
+
+exporter({
+  connectionString: connectionString
+//username: 'john'
+//password: 'T0pS3ecr3tP@ssw0rd'
+}, function(err, data) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data);
+  }
+});
